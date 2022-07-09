b5cc6ba96050a30611f9006cb5b70b92ff28114a
georgkromer <Georg.Kromer@nttdata.com>
2022-07-09 12:47:53 +0200

    waiting for braintree registration (if decline, rollback this)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,12 +3,10 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    BTAppContextSwitcher.setReturnURLScheme("com.example.ensobox.payments") // https://pub.dev/packages/flutter_braintree and https://developer.paypal.com/braintree/docs/guides/paypal/client-side/ios/v4
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
