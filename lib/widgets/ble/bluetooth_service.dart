ab1c0fa59f4efc2f99df9c35627b12c33e169b59
georgkromer <Georg.Kromer@nttdata.com>
2022-07-12 16:10:30 +0200

    feat: Showing Bluetooth status on Box Details screen works

--- lib/widgets/ble/bluetooth_service.dart
+++ lib/widgets/ble/bluetooth_service.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 class BluetoothService {
@@ -52,5 +54,38 @@ class BluetoothService {
 
   set connected(bool value) {
     _connected = value;
+  }
+
+  void handleError(Object e, BuildContext context) {
+    var re = RegExp(r'(?<=message: ")(.*)(?=\()');
+    log(e.toString());
+    var match = re.firstMatch(e.toString());
+    if (match != null) print(match.group(0));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content:
+          Text(match!.group(0) ?? "Bitte prüfen, ob Bluetooth aktiviert ist."),
+      duration: const Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'AKTIVIEREN',
+        onPressed: () {},
+      ),
+    ));
+
+    // String errorMessage =
+    //   switch (status) {
+    //     case BleStatus.unsupported:
+    //       return "This device does not support Bluetooth";
+    //     case BleStatus.unauthorized:
+    //       return "Authorize the FlutterReactiveBle example app to use Bluetooth and location";
+    //     case BleStatus.poweredOff:
+    //       return "Bluetooth is powered off on your device turn it on";
+    //     case BleStatus.locationServicesDisabled:
+    //       return "Enable location services";
+    //     case BleStatus.ready:
+    //       return "Bluetooth is up and running";
+    //     default:
+    //       return "Waiting to fetch Bluetooth status $status";
+    //   }
+    // }
   }
 }
--- lib/widgets/box_details_screen.dart
+++ lib/widgets/box_details_screen.dart
@@ -84,9 +84,19 @@ class _BoxDetailsScreenState extends State<BoxDetailsScreen> {
         setState(() {
           _bleService.discoveredDevice = device;
           _bleService.foundDeviceWaitingToConnect = true;
+          log('set _bleService.foundDeviceWaitingToConnect = true;');
         });
       });
     }
+  }
+
+  void _stopScan() async {
+    // We're done scanning, we can cancel it
+    _bleService.scanStream.cancel();
+    setState(() {
+      _bleService.scanStarted = false;
+    });
+    log('stopped scanning');
   }
 
   void _startScan() async {
@@ -119,6 +129,9 @@ class _BoxDetailsScreenState extends State<BoxDetailsScreen> {
 
         // log('device: $device.name');
         _addDeviceTolist(device);
+      }, onError: (Object e) {
+        log('Device scan fails with error: $e');
+        _bleService.handleError(e, context);
       });
     }
   }
@@ -182,19 +195,20 @@ class _BoxDetailsScreenState extends State<BoxDetailsScreen> {
   Widget build(BuildContext context) {
     // String itemUrl = widget.selectedBox.item_images?[0] ??
     //     'https://enso-box.s3.eu-central-1.amazonaws.com/Allura+-+Park.png';
-    List<String> itemImages = widget.selectedBox.item_images ?? [];
-    if (itemImages.length == 0) {
-      itemImages = [
-        'https://enso-box.s3.eu-central-1.amazonaws.com/Allura+-+Park.png'
-      ];
-    }
-
-    if (_bleService.scanStarted) {
+    // log();
+    // List<String> itemImages = widget.selectedBox.item_images ?? [];
+    // if (itemImages.length == 0) {
+    //   itemImages = [
+    //     'https://enso-box.s3.eu-central-1.amazonaws.com/Allura+-+Park.png'
+    //   ];
+    // }
+    if (!_bleService.scanStarted) {
       _startScan();
     }
     return WillPopScope(
       onWillPop: () async {
         _bleService.devicesList = [];
+        _stopScan();
         // Returning true allows the pop to happen, returning false prevents it.
         return true;
       },
@@ -211,73 +225,127 @@ class _BoxDetailsScreenState extends State<BoxDetailsScreen> {
               height: MediaQuery.of(context).size.height * .35,
               width: MediaQuery.of(context).size.width * 0.9,
               padding: const EdgeInsets.only(bottom: 30),
-              child: Image.network(itemImages[0]),
+              child: Image.network(widget.selectedBox.item_images != null &&
+                      widget.selectedBox.item_images!.isNotEmpty
+                  ? widget.selectedBox.item_images!.first
+                  : 'https://enso-box.s3.eu-central-1.amazonaws.com/Allura+-+Park.png'), //https://stackoverflow.com/questions/72951044/access-first-element-of-a-nullable-liststring-in-dart/72951153?noredirect=1#comment128852739_72951153
             ),
-            _buildListViewOfDevices(),
+            _createStatusTile(),
+            const Divider(
+              height: 1.0,
+            ),
           ],
         ),
-        persistentFooterButtons: [
-          // We want to enable this button if the scan has NOT started
-          // If the scan HAS started, it should be disabled.
-          _bleService.scanStarted
-              // True condition
-              ? ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.search),
-                )
-              // False condition
-              : ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: _startScan,
-                  child: const Icon(Icons.search),
+
+        // _buildListViewOfDevices(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.blue,
                 ),
-          _bleService.foundDeviceWaitingToConnect
-              // True condition
-              ? ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: _connectToDevice,
-                  child: const Icon(Icons.bluetooth),
-                )
-              // False condition
-              : ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.bluetooth),
+                label: "Zurück"),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.done_all,
+                  color: Colors.blue,
                 ),
-          _bleService.connected
-              // True condition
-              ? ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.blue, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: _partyTime,
-                  child: const Icon(Icons.celebration_rounded),
-                )
-              // False condition
-              : ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey, // background
-                    onPrimary: Colors.white, // foreground
-                  ),
-                  onPressed: () => goToPayment(context),
-                  child: const Icon(Icons.celebration_rounded),
-                ),
-        ],
+                label: "Jetzt ausleihen"),
+          ],
+          onTap: (int itemIndex) {
+            log("Pressed" + itemIndex.toString());
+            switch (itemIndex) {
+              case 0:
+                Navigator.pop(context);
+                break;
+              case 1:
+                log("Pressed 111" + itemIndex.toString());
+                _connectToDevice();
+                goToPayment(context);
+            }
+          },
+        ),
+        // persistentFooterButtons: [
+        //   // We want to enable this button if the scan has NOT started
+        //   // If the scan HAS started, it should be disabled.
+        //   _bleService.scanStarted
+        //       // True condition
+        //       ? ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.grey, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: () {},
+        //           child: const Icon(Icons.search),
+        //         )
+        //       // False condition
+        //       : ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.blue, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: _startScan,
+        //           child: const Icon(Icons.search),
+        //         ),
+        //   _bleService.foundDeviceWaitingToConnect
+        //       // True condition
+        //       ? ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.blue, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: _connectToDevice,
+        //           child: const Icon(Icons.bluetooth),
+        //         )
+        //       // False condition
+        //       : ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.grey, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: () {},
+        //           child: const Icon(Icons.bluetooth),
+        //         ),
+        //   _bleService.connected
+        //       // True condition
+        //       ? ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.blue, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: _partyTime,
+        //           child: const Icon(Icons.celebration_rounded),
+        //         )
+        //       // False condition
+        //       : ElevatedButton(
+        //           style: ElevatedButton.styleFrom(
+        //             primary: Colors.grey, // background
+        //             onPrimary: Colors.white, // foreground
+        //           ),
+        //           onPressed: () => goToPayment(context),
+        //           child: const Icon(Icons.celebration_rounded),
+        //         ),
+        // ],
       ),
     );
+  }
+
+  ListTile _createStatusTile() {
+    if (_bleService.foundDeviceWaitingToConnect) {
+      return new ListTile(
+        leading: const Icon(Icons.bluetooth_connected),
+        title: Text('Bluetooth Verbindung hergestellt'),
+        subtitle: Text(
+            'Dein Handy konnte eine Bluetooth Verbindung zur ${widget.selectedBox.name} herstellen.'),
+      );
+    } else {
+      return new ListTile(
+        leading: const Icon(Icons.bluetooth_searching),
+        title: Text('Keine Bluetooth Verbindung'),
+        subtitle: Text(
+            'Dein Handy konnte keine Bluetooth Verbindung zur ${widget.selectedBox.name} herstellen. Hast du dein Bluetooth aktiviert?'),
+      );
+    }
   }
 }
