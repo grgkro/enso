2601e59d43710d78f369026274144f731e761876
georgkromer <Georg.Kromer@nttdata.com>
2022-07-18 10:59:08 +0200

    feat: taking and displaying a picture works

--- lib/widgets/camera/takePictureScreen.dart
+++ lib/widgets/camera/takePictureScreen.dart
@@ -1,11 +1,16 @@
+import 'dart:developer';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
+import 'display_picture_screen.dart';
+
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription camera;
+
   TakePictureScreen({required this.camera});
 
   @override
@@ -38,6 +43,46 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
           }
         },
       ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: FloatingActionButton(
+          // Provide an onPressed callback.
+          onPressed: () async {
+            // Take the Picture in a try / catch block. If anything goes wrong,
+            // catch the error.
+            try {
+              // Ensure that the camera is initialized.
+              await _initializeCameraControllerFuture;
+              XFile image;
+              // Attempt to take a picture and then get the location
+              // where the image file is saved.
+              if (_cameraController != null) {
+                image = await _cameraController!.takePicture();
+              } else {
+                log("Tried to takePicture before _cameraController was initialized - _initializeCameraControllerFuture was wrongly true");
+                return;
+              }
+
+              if (!mounted) return;
+
+              // If the picture was taken, display it on a new screen.
+              await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen(
+                    // Pass the automatically generated path to
+                    // the DisplayPictureScreen widget.
+                    imagePath: image.path,
+                  ),
+                ),
+              );
+            } catch (e) {
+              // If an error occurs, log the error to the console.
+              print(e);
+            }
+          },
+          child: const Icon(Icons.camera_alt),
+        ),
+      )
     ]);
   }
 
